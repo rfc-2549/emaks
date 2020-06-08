82b5995265723effb557d684ab6c34dccda431a0
qorg11 <qorg@vxempire.xyz>
2020-06-08 17:13:20 +0200

    idk i just added hl-line-mode and added default erc server

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "18cd5a0173772cdaee5522b79c444acbc85f9a06055ec54bb91491173bc90aaa" default)))
  '(package-selected-packages
    (quote
-    (sudo-edit swiper solarized-theme humanoid-themes humanoid-theme laguna-theme which-key spacemacs-theme use-package minsk-theme auctex htmlize plan9-theme zencoding-mode wdl-mode visual-fill-column twittering-mode tramp-term tracking tetris ssh srv smiles-mode smex slime seti-theme scheme-complete s rust-mode restclient projectile plsense pdf-tools org-bullets nav-flash mutt-mode multiple-cursors markdown-mode magit ivy-youtube haskell-mode flycheck evil emms diredfl dired-details dashboard cyberpunk-theme button-lock all-the-icons))))
+    (go-mode sudo-edit swiper solarized-theme humanoid-themes humanoid-theme laguna-theme which-key spacemacs-theme use-package minsk-theme auctex htmlize plan9-theme zencoding-mode wdl-mode visual-fill-column twittering-mode tramp-term tracking tetris ssh srv smiles-mode smex slime seti-theme scheme-complete s rust-mode restclient projectile plsense pdf-tools org-bullets nav-flash mutt-mode multiple-cursors markdown-mode magit ivy-youtube haskell-mode flycheck evil emms diredfl dired-details dashboard cyberpunk-theme button-lock all-the-icons))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
